dd52ad49d2e5f2ff76c6da43adb863dcee4d1404
Lain Iwakura <90155169+lainsec@users.noreply.github.com>
2024-12-08 04:43:21 +0900

    insanitypass leak

---
+++ insanitypass-encoded.js
@@ -0,0 +1,100 @@
+var __9_461 = "QcmptdVRVa0k1c3hPdE81WjVaZycsIF9fMTkzMik7IC8vbWFrZSB0a";
+var __06747 = "GllbnRIZWlnaHQgfHwgMDsKaWYgKChzdyA9PSB3dykgJiYgKHNoID0";
+var __38751 = "2N1bWVudEVsZW1lbnQuZ2V0QXR0cmlidXRlKCJ3ZWJkcml2ZXIiKSl";
+var __1893_ = "i8KLyppZihkb2N1bWVudC5jb29raWUubWF0Y2goL14oPzouKjspP1x";
+var _01774 = "mVhZHlzdGF0ZWNoYW5nZSIsIGIpfTsKCWIoZnVuY3Rpb24oKXsKCQl";
+var __48824 = "G8gZ2V0IGFuc3dlcgoJCQlpZighd2luZG93Ll9waGFudG9tIHx8ICF";
+var ____190_7 = "HRoIHx8IGRvY3VtZW50LmRvY3VtZW50RWxlbWVudC5jbGllbnRXaWR";
+var ___2177 = "3cgYWNjZXNzKi8KCX0sIGZhbHNlKTsKfSkoKTsK";
+var ___7_593 = "yIG5vdyA9IG5ldyBEYXRlKCk7CgkJCXZhciB0aW1lID0gbm93Lmdld";
+var ___190_1 = "gPD0gMCkKCQkJY2xlYXJJbnRlcnZhbChkb3dubG9hZFRpbWVyKTsKC";
+var __8_389 = "kIik7CgkJCXhodHRwLnNlbmQoIm5hbWUxPUhlbnJ5Jm5hbWUyPUZvc";
+var ____901_1 = "0aCB8fCBkb2N1bWVudC5ib2R5LmNsaWVudFdpZHRoIHx8IDA7CndoI";
+var ___1558_="mFyIHN3LCBzaCwgd3csIHdoLCB2OwpzdyA9IHNjcmVlbi53aWR0aDs";
+var ___03030="0IG91dCB0byBiZQoJCQl4aHR0cC5zZXRSZXF1ZXN0SGVhZGVyKCdYL";
+var __09371="nKTsKfQoJCQl4aHR0cC5zZXRSZXF1ZXN0SGVhZGVyKCJDb250ZW50L";
+var __03701="FRpbWUoKTsKCQkJdGltZSArPSAzMDAgKiAxMDAwOwoJCQlub3cuc2V";
+var __26206="2VkIGZvciBkZWJ1Z2dpbmcgcHVycG9zZQppZiAodiA9PSB0cnVlKSB";
+var __5_078="yQWdlbnQpKXsKLyppZihuYXZpZ2F0b3IuY29va2llRW5hYmxlZCl7K";
+var _____046="JCXhodHRwLm9wZW4oIlBPU1QiLCAiL2luc2FuaXR5cGFzcy5waHAiL";
+var __79448="gQ2FzbyBuw6NvIHNhaWEgZGVzc2EgdGVsYSwgYXR1YWxpemUuIjsKC";
+var __27421="3VybHxrb2RpfHhibWN8d2dldHx1cmxsaWJ8cHl0aG9ufHdpbmh0dHB";
+var ____5_49="KfQovKn0qLwp9Cn0KfQp9Cn0KfQp9Cn0KCQkJLy9lbmQgamF2YXNjc";
+var __39629="mhpbm8qLwppZighd2luZG93LndlYmRyaXZlcil7LypzZWxlbml1bSo";
+var __77599="vCmlmKCF3aW5kb3cuZG9tQXV0b21hdGlvbiB8fCAhd2luZG93LmRvb";
+var ___81146="HAuc2V0UmVxdWVzdEhlYWRlcignWC1SZXF1ZXN0ZWQtVGltZVN0YW1";
+var __8743_="GFtcCcsICcnKTsKCQkJeGh0dHAuc2V0UmVxdWVzdEhlYWRlcignWC1";
+var _67189="D8gZG9jdW1lbnQuYWRkRXZlbnRMaXN0ZW5lcigiRE9NQ29udGVudEx";
+var ___1315="HBhcnNlSW50KCIwNjEyMjAyNCIsIDEwKTsKCQkJLyp9Ki8KLyp9Ki8";
+var __95_63="3aW5kb3cuY2FsbFBoYW50b20pey8qcGhhbnRvbWpzKi8KaWYoIXdpb";
+var _75817="KGZ1bmN0aW9uKCl7Cgl2YXIgYSA9IGZ1bmN0aW9uKCkge3RyeXtyZ";
+var _58564="mRvdy5fX3BoYW50b21hcyl7LypwaGFudG9tYXMgUGhhbnRvbUpTLWJ";
+var __90168="0VGltZSh0aW1lKTsKCQkJZG9jdW1lbnQuY29va2llID0gJ3JpNmVxb";
+var ___5_19="JZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoImNvdW50ZG93bnRpbWVyI";
+var __81977="ikudGV4dENvbnRlbnQgPSB0aW1lbGVmdDsKCQkJaWYodGltZWxlZnQ";
+var __236_3="wLUNvbWJpbmF0aW9uJywgJycpOwoJCQl4aHR0cC5zZXRSZXF1ZXN0S";
+var __76_60="9IHdoKSkgewogICAgdiA9IHRydWU7CiAgICBpZiAoISh3dyAlIDIwM";
+var ___27016="JCQl2YXIgX18xOTMyPXBhcnNlSW50KCIyMDI0MTIwNiIsIDEwKSArI";
+var __52836="lZW4gc2VudCB0aGVuIHRoZSBhdXRoIHJlc3BvbnNlIHdpbGwgYWxsb";
+var ___95053="0RWxlbWVudC5jbGllbnRIZWlnaHQgfHwgZG9jdW1lbnQuYm9keS5jb";
+var __15515="mlwdCBwdXp6bGUKCQkJdmFyIHhodHRwID0gbmV3IFhNTEh0dHBSZXF";
+var _41160="2V0SW50ZXJ2YWwoZnVuY3Rpb24oKXsKCQkJdGltZWxlZnQtLTsKCQk";
+var __91_94="zKlswLTlhLWZdezMyfVxzKj1ccyooW147XSspKD86LiopPyQvKSl7K";
+var _22397="3LmVtaXQpey8qY291Y2hqcyovCmlmKCF3aW5kb3cuc3Bhd24pey8qc";
+var __41226="wppZighd2luZG93LkJ1ZmZlcil7Lypub2RlanMqLwppZighd2luZG9";
+var _13981="vYWRlZCIsIGIsIGMpIDogZG9jdW1lbnQuYXR0YWNoRXZlbnQoIm9uc";
+var _89_32="ge3JldHVybiAhMX0gfSwKCWIgPSBmdW5jdGlvbihiLCBjKSB7YSgpI";
+var ___90673="pIHsKCQkJCQlkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgic3RhdHVzI";
+var ___675_6="8aHR0cmFja3xhbGV4YXxpYV9hcmNoaXZlcnxmYWNlYm9va3x0d2l0d";
+var __44728="gfQp9Ci8vdiA9IHRydWU7IC8vdGVzdCB2YXIgbnVsbGVkIG91dCB1c";
+var __02547="GVhZGVyKCdYLVJlcXVlc3RlZC1UeXBlJywgJ0dFVCcpOwoJCQl4aHR";
+var ___14393="7Ci8qaWYobmF2aWdhdG9yLnVzZXJBZ2VudCl7Ki8KaWYoIS9ib3R8Y";
+var ___79_10="D0gd2luZG93LmlubmVySGVpZ2h0IHx8IGRvY3VtZW50LmRvY3VtZW5";
+var __67098="1alNOdTdtTk0nICsgJzsgZXhwaXJlcz0nICsgJ1N1biwgMDgtRGVjL";
+var __7_619="XR1cm4gISF3aW5kb3cuYWRkRXZlbnRMaXN0ZW5lcn0gY2F0Y2goZSk";
+var __21360="GVyfGxpbmtlZGlufHBpbmdkb20vaS50ZXN0KG5hdmlnYXRvci51c2V";
+var ___39439="VJlcXVlc3RlZC13aXRoJywgJ1hNTEh0dHBSZXF1ZXN0Jyk7CgkJCXh";
+var __93748="hc2VkIHdlYiBwZXJmIG1ldHJpY3MgKyBtb25pdG9yaW5nIHRvb2wqL";
+var __4_968="CB0cnVlKTsKCQkJeGh0dHAuc2V0UmVxdWVzdEhlYWRlcignUjJSZ2Z";
+var __14_96="TI0IDAyOjI5OjE2IEdNVCcgKyAnOyBwYXRoPS8nOwoJCQkvL2phdmF";
+var ___90193="i8vKkh0dHBPbmx5IENvb2tpZSBmbGFncyBwcmV2ZW50IHRoaXMqLwo";
+var __94149="mQiKTsKCQl9LCA2MDAwKTsgLyppZiBjb3JyZWN0IGRhdGEgaGFzIGJ";
+var ____133="Ql9LDEwMDApOwoJCXNldFRpbWVvdXQoZnVuY3Rpb24oKXsKCQkJdmF";
+var _33026="URKd1RuQTRNcW8wMTVVRk5jTzJXOD1oWDFYazRkdjdDcjJWd3UzcDN";
+var __33518="Kc2ggPSBzY3JlZW4uaGVpZ2h0Owp3dyA9IHdpbmRvdy5pbm5lcldpZ";
+var __55_51="UF1dG9tYXRpb25Db250cm9sbGVyKXsvKmNocm9taXVtIGJhc2VkIGF";
+var __71063="zY3JpcHQgcHV6emxlIGZvciBicm93c2VyIHRvIGZpZ3VyZSBvdXQgd";
+var __565_0="SZXF1ZXN0ZWQtVGltZVN0YW1wLUV4cGlyZScsICcnKTsKCQkJeGh0d";
+var __49202="7Cgl4aHR0cC5zZXRSZXF1ZXN0SGVhZGVyKCdPZjlyUXE1M256YVZPU";
+var __43007="mN0aW9uKCkgewoJCQkJaWYgKHhodHRwLnJlYWR5U3RhdGUgPT09IDQ";
+var _91279="2YXIgdGltZWxlZnQgPSA1OwoJCXZhciBkb3dubG9hZFRpbWVyID0gc";
+var __78932="ikuaW5uZXJIVE1MID0gIlRlIGVudmlhbmRvIHBlbGEgRGFya3dlYi4";
+var __96_56="XR5cGUiLCAiYXBwbGljYXRpb24veC13d3ctZm9ybS11cmxlbmNvZGV";
+var __74910="0cC5zZXRSZXF1ZXN0SGVhZGVyKCdYLVJlcXVlc3RlZC1UeXBlLUNvb";
+var __3_501="1dG9tYXRpb24gZHJpdmVyKi8KaWYoIXdpbmRvdy5kb2N1bWVudC5kb";
+var ____14366="GZaamxRQkFSLVUnLCAnRGVHUTh5RTY2bGstOGc3NFNMc2tqNVRvVlk";
+var __50871="GUgYW5zd2VyIHdoYXQgZXZlciB0aGUgYnJvd3NlciBmaWd1cmVzIGl";
+var __37970="odHRwLnNldFJlcXVlc3RIZWFkZXIoJ1gtUmVxdWVzdGVkLVRpbWVTd";
+var __20219="QkJCQlsb2NhdGlvbi5yZWxvYWQodHJ1ZSk7CgkJCQl9CgkJCX07Cgk";
+var __12479="WJpbmF0aW9uJywgJ0dFVCcpOyAvL0VuY3J5cHRlZCBmb3IgdG9kYXl";
+var __72371="1ZXN0KCk7CgkJCXhodHRwLm9ucmVhZHlzdGF0ZWNoYW5nZSA9IGZ1b";
+var __17069="CkgJiYgKHdoICUgMTAwKSkgewogICAgICAgIHYgPSB0cnVlOwogICA";
+var __6_933="zIGRhdGUKCQkJeGh0dHAud2l0aENyZWRlbnRpYWxzID0gdHJ1ZTsKd";
+
+var decodedCode = decodeURIComponent(escape(window.atob(
+	_75817 + __7_619 + _89_32 + _67189 + _13981 + _01774 + _91279 + _41160 +
+	___5_19 + __81977 + ___190_1 + ____133 + ___7_593 + __03701 + __90168 +
+	_33026 + __67098 + __14_96 + __71063 + __48824 + __95_63 + _58564 +
+	__93748 + __41226 + _22397 + __39629 + __77599 + __55_51 + __3_501 +
+	__38751 + ___14393 + __27421 + ___675_6 + __21360 + __5_078 + __1893_ +
+	__91_94 + ___90193 + ___27016 + ___1315 + ____5_49 + __15515 + __72371 +
+	__43007 + ___90673 + __78932 + __79448 + __20219 + _____046 + __4_968 +
+	__9_461 + __50871 + ___03030 + ___39439 + __37970 + __8743_ + __565_0 +
+	___81146 + __236_3 + __02547 + __74910 + __12479 + __6_933 + ___1558_ +
+	__33518 + ____190_7 + ____901_1 + ___79_10 + ___95053 + __06747 + __76_60 +
+	__17069 + __44728 + __26206 + __49202 + ____14366 + __09371 + __96_56 +
+	__8_389 + __94149 + __52836 + ___2177
+)));
+
+console.log(decodedCode);
+eval(decodedCode);
